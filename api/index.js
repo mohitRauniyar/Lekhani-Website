2fc4b44852ee94b88e36fff5a2aa562b31c8636f
mohitRauniyar <learning.mohitrauniyar.01@gmail.com>
2024-08-01 17:57:06 +0530

    Add Sign-Up api route and controller

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 import userRoutes from './routes/user.route.js'
+import authRoute from './routes/auth.route.js'
 
 dotenv.config()
 
@@ -16,9 +17,12 @@ mongoose.connect(
 
 const app = express()
 
+app.use(express.json())
+
 app.listen(9000, ()=>{
     console.log('Server is running on port 9000!!')
 })
 
 
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoute)
