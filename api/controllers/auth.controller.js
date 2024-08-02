921bdf90c7344299f08749a303fbe720d6ed1c48
mohitRauniyar <learning.mohitrauniyar.01@gmail.com>
2024-08-02 19:14:22 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs'
 import User from '../models/user.model.js'
 import { errorHandler } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 
 export const signup = async (req, res, next)=>{
@@ -18,6 +19,41 @@ export const signup = async (req, res, next)=>{
     try {
         await newUser.save()
         res.json("Signup successful")
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+
+export const signin = async (req, res, next)=>{
+    const {email, password} = req.body
+
+    if(!email || !password || email === '' || password ===''){
+        next(errorHandler(400, 'All fields are required'))
+    }
+
+    try {
+        const validUser = await User.findOne({email})
+        if(!validUser){
+            return next(errorHandler(404, 'Incorrect Credentials'))
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if(!validPassword){
+            return next(errorHandler(400, 'Incorrect Credentials'))
+        }
+
+        const token = jwt.sign(
+            {id: validUser._id},
+            process.env.JWT_SECRET_KEY
+        )
+
+        const {password: pass, ...rest} = validUser._doc
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json(rest)
     } catch (error) {
         next(error)
     }
