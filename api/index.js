c90ee235e8e3a54b1550225ffd0183ce4b45d08c
mohitRauniyar <learning.mohitrauniyar.01@gmail.com>
2024-08-24 18:42:03 +0530

    update user api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 
 import userRoutes from './routes/user.route.js'
 import authRoute from './routes/auth.route.js'
@@ -18,6 +19,7 @@ mongoose.connect(
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.listen(9000, ()=>{
     console.log('Server is running on port 9000!!')
